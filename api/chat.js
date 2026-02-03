3f3b202ab144ea818efd339a62a02ebfa6f0e527
novadapurwadi <novada_persona3@yahoo.com>
2026-02-03 22:20:57 +0700

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -38,8 +38,8 @@ module.exports = async (req, res) => {
     const nimRequest = {
       model: nimModel,
       messages: messages,
-      temperature: temperature || 0.6,
-      max_tokens: max_tokens || 9024,
+      temperature: temperature || 0.8,
+      max_tokens: max_tokens || 13000,
       extra_body: ENABLE_THINKING_MODE ? { chat_template_kwargs: { thinking: true } } : undefined,
       stream: false // Vercel doesn't handle streaming well
     };
